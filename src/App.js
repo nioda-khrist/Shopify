e42f5cb1e7757fdc9dba662f172953c82d17ea12
Khrist <khrist.nioda@gmail.com>
2021-01-09 18:43:43 +0800

    added shopify branch

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import firebase from './config/fbConfig';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ProtectedRoute } from './components';
+import { ProtectedRoute,404 } from './components';
 import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
 
 import Home from './pages/Home';
 import Settings from './pages/Settings';
+import Products from './pages/Products';
 
 const rrfProps = {
   firebase,
@@ -38,6 +39,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <ProtectedRoute path='/settings' component={Settings} />
+                <Route path='/products' component={Products} />
+                <Route path='*' component={404} />
               </Switch>
             </Router>
           </main>
